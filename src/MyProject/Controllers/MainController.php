4aceae88040b880e89611607921e2eb2f2d92749
Vadim Romanov <spbromanov@mail.ru>
2020-03-19 14:02:06 +0300

    add main controller

---
+++ src/MyProject/Controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: market7
+ * Date: 19.03.2020
+ * Time: 13:56
+ */
+
+namespace MyProject\Controllers;
+
+class MainController
+{
+
+    public function main()
+    {
+        echo 'Главная станица';
+    }
+
+}
--- www/index.php
+++ www/index.php
@@ -1,19 +1,18 @@
 <?php
 
-//require_once __DIR__ . '/src/MyProject/Models/Users/User.php';
-//require_once __DIR__ . '/src/MyProject/Models/Articles/Article.php';
 function myAutoLoader(string $className){
     //var_dump($className);
-    require_once __DIR__ . '/src/' . $className . '.php';
+    require_once __DIR__ . '/../src/' . $className . '.php';
 }
 
 spl_autoload_register('myAutoLoader');
 
 
 
-
+$controller = new \MyProject\Controllers\MainController();
+$controller->main();
 $author = new MyProject\Models\Users\User('Nike', 'admin');
 $article = new MyProject\Models\Articles\Article('Заголово', 'Текст', $author);
-echo 'Имя автора ' . $article->getAuthor()->getName();
+//echo 'Имя автора ' . $article->getAuthor()->getName();
 //var_dump($article);
 
