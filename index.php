1744f304ee87239cf6e422155adc4df8e7ec2d29
Vadim Romanov <spbromanov@mail.ru>
2020-03-19 10:02:44 +0300

    new branch dev

--- index.php
+++ index.php
@@ -1,71 +1,31 @@
 <?php
 
-interface calculateSquare {
-    public function calculateSquare();
-}
-
-
-class Rectangle
+trait SayYourClassTrait
 {
-    private $x;
-    private $y;
-
-    public function __construct($x, $y){
-        $this->x = $x;
-        $this->y = $y;
-    }
-
-    public function calculateSquare(){
-        return $this->x * $this->y;
-
+    public function SayYourClass(){
+        return 'My class is ' . self::class;
     }
 }
 
-class Square implements calculateSquare
+interface ISayYourClass{
+    public function SayYourClass();
+}
+
+class Man implements ISayYourClass
 {
-    private $x;
-
-    public function __construct($x){
-        $this->x = $x;
-    }
-    public function calculateSquare(){
-        return $this->x **2;
-    }
+    use SayYourClassTrait;
 }
 
-class Circle implements calculateSquare
+class Box implements ISayYourClass
 {
-    const PI =  3.14;
-    private $r;
-
-
-    public function __construct($r){
-        $this->r = $r;
-    }
-
-    public function calculateSquare(){
-
-        return self::PI * ( $this->r ** 2);
+    public function SayYourClass(){
+        return 'My class is ' . self::class;
     }
 }
 
-$circle = new Circle(2.5);
 
-
-$objects = [
-  new Square(5),
-    new Rectangle(2,4),
-    new Circle (5)
-];
-
-
-foreach($objects as $object){
-    if($object instanceof CalculateSquare){
-        echo 'Объект реализует интерфейс CalculateSquqre. Площадь: ' . $object->calculateSquare();
-        echo "<br/>";
-    }else echo 'Объект класса ' .  get_class($object) . ' не реализует интерфейс CalculateSquare<br/>';
-}
-
-
-
-//var_dump($circle instanceof CalculateSquare);
+$man = new Man();
+$box = new Box();
+echo $man->SayYourClass();
+echo "<br/>";
+echo $box->SayYourClass();
