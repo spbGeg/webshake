418e94c590bea5b293bc04c93b8c01b988da191f
Vadim <spbromanov@mail.ru>
2020-04-17 11:31:32 +0300

    under merge developer branch

--- templates/footer.php
+++ templates/footer.php
@@ -3,7 +3,7 @@
 <td width="300px" class="sidebar">
     <div class="sidebarHeader">Меню</div>
     <ul>
-        <li><a href="/">Главная страница</a></li>
+        <li><a href="/www">Главная страница</a></li>
         <li><a href="/about-me">Обо мне</a></li>
     </ul>
 </td>
